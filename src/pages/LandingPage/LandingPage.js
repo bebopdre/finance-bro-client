3dbc0c5cf50662f0a1e104c3c604942586d3add1
andreaairplane <andreadrematos@gmail.com>
2023-09-17 02:22:24 -0400

    finished landing

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -3,24 +3,21 @@ import "../LandingPage/LandingPage.scss";
 const LandingPage = () => {
   return (
     <div className="landing">
-      <div className="landing___title-container">
-        <h1 className="landing__title"> Shell City Trails</h1>
-        <div className="landing__subtitle-container">
-          <h2 className="landing_subtitle">The financial literacy game.</h2>
+      <section className="landing__background">
+        <div className="landing__title-container">
+          <h1 className="landing__title"> Shell City Trails</h1>
+          <div className="landing__subtitle-container">
+            <h2 className="landing__subtitle">The financial literacy game.</h2>
+          </div>
         </div>
-      </div>
 
-      <div className="landing__buttons">
-        <div className="landing__item">
-          <button className="landing__button"> New Game</button>
+        <div className="landing__buttons">
+            <button className="landing__button"> New Game</button>
+            <button className="landing__button"> Save Game</button>
+            <button className="landing__button"> Exit</button>
         </div>
-        <div className="landing__item">
-          <button className="landing__button"> Save Game</button>
-        </div>
-        <div className="landing__item">
-          <button className="landing__button"> Exit</button>
-        </div>
-      </div>
+      </section>
+      
     </div>
   );
 };
